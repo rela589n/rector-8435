340268414d9713f0a94196f4ccf3ea7ee499603c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-01-30 17:10:42 +0700

    Real fix solution for load phpunit.phar

--- rector.php
+++ rector.php
@@ -9,8 +9,14 @@ use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([__DIR__.'/']);
-    $rectorConfig->skip([__DIR__.'/vendor']);
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->bootstrapFiles([
+        __DIR__ . '/bin/phpunit.phar',
+    ]);
 
     $rectorConfig->rules([
         TypedPropertyFromAssignsRector::class,
