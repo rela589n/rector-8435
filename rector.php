cdd34b9216bbe38e3ce1eccb950cd732cff77fa0
Tomas Votruba <tomas.vot@gmail.com>
2024-01-31 09:55:34 +0100

    cleanup

--- rector.php
+++ rector.php
@@ -6,16 +6,13 @@ use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector;
 use Rector\Privatization\Rector\ClassMethod\PrivatizeFinalClassMethodRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
-use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([__DIR__.'/']);
-    $rectorConfig->skip([__DIR__.'/vendor']);
+    $rectorConfig->paths([__DIR__.'/src']);
 
     $rectorConfig->rules([
         TypedPropertyFromAssignsRector::class,
         RemoveParentCallWithoutParentRector::class,
         PrivatizeFinalClassMethodRector::class,
     ]);
-    $rectorConfig->phpVersion(PhpVersion::PHP_80);
 };
